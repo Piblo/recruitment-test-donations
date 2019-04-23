a73915040f3e9f97d8c0426b3f9301848981565f
Pablo Guerrero <pablo.orozco@kpmg.co.uk>
2019-04-23 18:23:43 +0100

    Added storybook, jes, babel and dotenv

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,12 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new Dotenv({
+      path: './config/.env'
+    })
   ]
 };
---
+++ src/components/DonationCard/DonationCard.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react/dist/client/preview';
+
+storiesOf('DonationCard', module)
+  .add('default', () => (
+    <div>hello</div>
+  ));
